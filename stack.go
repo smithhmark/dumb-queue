35c41cca11660f3724effd3f3f13c9763c21e309
Mark Smith <smith.h.mark@gmail.com>
2018-03-13 16:44:54 -0700

    adding external Stack

---
+++ stack.go
@@ -0,0 +1,42 @@
+package dumbqueue
+
+import "errors"
+
+type Stack struct {
+	head   *node
+	length int
+}
+
+type node struct {
+	value interface{}
+	next  *node
+}
+
+func NewStack() *Stack {
+	s := &Stack{nil, 0}
+	return s
+}
+
+func newNode(item interface{}, next *node) *node {
+	n := &node{item, next}
+	return n
+}
+
+func (s *Stack) Size() int {
+	return s.length
+}
+
+func (s *Stack) Push(item interface{}) {
+	newHead := newNode(item, s.head)
+	s.head, s.length = newHead, s.length+1
+}
+
+func (s *Stack) Pop() (interface{}, error) {
+	if s.length == 0 {
+		return nil, errors.New("Pop on empty Stack")
+	}
+	np := s.head
+	s.head = np.next
+	s.length--
+	return np.value, nil
+}
