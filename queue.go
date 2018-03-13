cf991a5aee1c58c731ac611fbe5d7a31d29df2e5
Mark Smith <smith.h.mark@gmail.com>
2018-03-13 16:44:25 -0700

    fmting

--- queue.go
+++ queue.go
@@ -4,35 +4,33 @@ package dumbqueue
 import "errors"
 
 type SlowGetQueue struct {
-        a []int
-        b []int
+	a []int
+	b []int
 }
 
 func (q *SlowGetQueue) Put(ii int) {
-        q.a = append(q.a, ii);
+	q.a = append(q.a, ii)
 }
 
 func (q *SlowGetQueue) Get() (ret int, err error) {
-        if len(q.a) == 0 {
-                return 0, errors.New("Queue is empty")
-        }
-        err = nil
-        for _, ii := range q.a {
-                q.b = append(q.b, ii)
-        }
-        q.a = q.a[:0]
-        // q.a = nil
+	if len(q.a) == 0 {
+		return 0, errors.New("Queue is empty")
+	}
+	err = nil
+	for _, ii := range q.a {
+		q.b = append(q.b, ii)
+	}
+	q.a = q.a[:0]
+	// q.a = nil
 
-        ret = q.b[len(q.b)-1];
+	ret = q.b[len(q.b)-1]
 
-        q.b = q.b[:len(q.b)-1];
+	q.b = q.b[:len(q.b)-1]
 
-        for _, ii := range q.b {
-                q.a = append(q.a, ii);
-        }
-        q.b = q.b[:0]
-        // q.b = nil
-        return
+	for _, ii := range q.b {
+		q.a = append(q.a, ii)
+	}
+	q.b = q.b[:0]
+	// q.b = nil
+	return
 }
-
-
--- queue_test.go
+++ queue_test.go
@@ -1,51 +1,51 @@
 package dumbqueue
 
 import (
-        "testing"
-        //"fmt"
+	"testing"
+	//"fmt"
 )
 
 func TestSPQConstruction(t *testing.T) {
-        //fmt.Println("start")
-        q  := new(SlowGetQueue)
-        if len(q.a) != 0 || len(q.b) != 0 {
-                t.Fatalf("new queue has stuff in it")
-        }
+	//fmt.Println("start")
+	q := new(SlowGetQueue)
+	if len(q.a) != 0 || len(q.b) != 0 {
+		t.Fatalf("new queue has stuff in it")
+	}
 }
 
 func TestSPQPutOneItem(t *testing.T) {
-        //fmt.Println("start")
-        q  := new(SlowGetQueue)
-        q.Put(0)
-        if len(q.a) != 1 && len(q.b) != 0{
-                t.Fatalf("Put lost item")
-        }
+	//fmt.Println("start")
+	q := new(SlowGetQueue)
+	q.Put(0)
+	if len(q.a) != 1 && len(q.b) != 0 {
+		t.Fatalf("Put lost item")
+	}
 }
 
 func TestSPQErrorOnEmpty(t *testing.T) {
-        q  := new(SlowGetQueue)
-        _, err := q.Get()
-        if err == nil {
-                t.Fatalf("Get on empty queue should error")
-        }
+	q := new(SlowGetQueue)
+	_, err := q.Get()
+	if err == nil {
+		t.Fatalf("Get on empty queue should error")
+	}
 }
 
 func TestSPQPutOneGetOneItem(t *testing.T) {
-        //fmt.Println("start")
-        q  := new(SlowGetQueue)
-        item := 0
-        q.Put(item)
-        if len(q.a) != 1 && len(q.b) != 0{
-                t.Fatalf("Put lost item")
-        }
-        ii, err := q.Get()
-        if err != nil {
-                t.Fatalf("Get should work if theres data in the queue")
-        }
-        if ii != item {
-                t.Fatalf("Get did not return item")
-        }
-        if len(q.a) != 0 && len(q.b) != 0{
-                t.Fatalf("Get didn't clear out inventory")
-        }
+	//fmt.Println("start")
+	q := new(SlowGetQueue)
+	item := 0
+	q.Put(item)
+	if len(q.a) != 1 && len(q.b) != 0 {
+		t.Fatalf("Put lost item")
+	}
+	ii, err := q.Get()
+	if err != nil {
+		t.Fatalf("Get should work if theres data in the queue")
+	}
+	if ii != item {
+		t.Fatalf("Get did not return item")
+	}
+	if len(q.a) != 0 && len(q.b) != 0 {
+		t.Fatalf("Get didn't clear out inventory")
+	}
 }
